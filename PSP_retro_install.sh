4fb606eafccf18862411189427b7afd0aa5eab32
gxk2000 <77479970+gxk2000@users.noreply.github.com>
2022-09-08 19:01:12 +0800

    Update PSP_retro_install.sh

--- PSP_retro_install.sh
+++ PSP_retro_install.sh
@@ -2,43 +2,44 @@
 
 # Download nds_install_packet files
 cd ~
-git clone https://gitee.com/KXGL/retro_game_psp_install.git
-cd ~/retro_game_psp_install/
+git clone https://github.com/gxk2000/Retro_Game_PSP_Install.git
+cd ~/Retro_Game_PSP_Install/
 unzip psp_install_packet.zip
 
 # move file
-cd ~/retro_game_psp_install/
-sudo mv "/home/cpi/retro_game_psp_install/PSP.png" "/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/PSP.png"
-sudo mv "/home/cpi/retro_game_psp_install/12_PSP/" "/home/cpi/apps/Menu/20_Retro Games/12_PSP/"
-sudo mv "/home/cpi/retro_game_psp_install/psp-1.12/" "/home/cpi/psp-1.12"
+cd ~/Retro_Game_PSP_Install/
+sudo mv "/home/cpi/Retro_Game_PSP_Install/PSP.png" "/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/PSP.png"
+sudo mv "/home/cpi/Retro_Game_PSP_Install/12_PSP/" "/home/cpi/apps/Menu/20_Retro Games/12_PSP/"
+sudo mv "/home/cpi/Retro_Game_PSP_Install/psp-1.12/" "/home/cpi/psp-1.12"
 sudo chmod +x "/home/cpi/psp-1.12/PPSSPPSDL"
 
 # tip
-printf "\e[33m Warning: Make sure you have mesa 20 or later installed, otherwise you will not be able to run NDS rom!!!For details, please see : https://gitee.com/KXGL/retro_game_psp_install"
-
+printf "\n"
+printf "\e[33m Warning: Make sure you have mesa 20 or later installed, otherwise you will not be able to run PSP rom!!!For details, please see : https://gitee.com/KXGL/Retro_Game_PSP_Install \033[0m"
+printf "\n"
 Path1="/home/cpi/apps/Menu/20_Retro Games/12_PSP/"
 Path2="/home/cpi/psp-1.12/"
 File1="/home/cpi/launcher/skin/default/Menu/GameShell/20_Retro Games/PSP.png"
 
 if [[ ! -d "$Path1" ]]; then
 
-    printf "！！！！！！！！！！Installation Failed !！！！！！！！！！！\n"
+    printf "Installation Failed !\n"
 
 else
 
     if [[ ! -d "$Path2" ]]; then
 
-        printf "！！！！！！！！！！Installation Failed !！！！！！！！！！！\n"
+        printf "Installation Failed !\n"
 
     else
 
         if [[ ! -e "$File1" ]]; then
 
-            printf "！！！！！！！！！！Installation Failed !！！！！！！！！！！\n"
+            printf "Installation Failed !\n"
 
         else
 
-            printf "！！！！！！！！！！Installation complete !！！！！！！！！！！\n"
+            printf "Installation complete !\n"
 
         fi
 
@@ -48,4 +49,4 @@ fi
 
 # delete file
 cd ~
-sudo rm -rf ~/retro_game_psp_install/
+sudo rm -rf ~/Retro_Game_PSP_Install/
